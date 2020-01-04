3a90389d377da37514b609c8bd2eb92d755493d9
vos-hub <56769051+vos-hub@users.noreply.github.com>
2020-01-04 21:44:39 +0100

    Created function for search country by click on button and modified version of app.js.

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,5 @@
 'use strict';
 
-const weatherApp = {
-  selectedLocations: {},
-  addDialogContainer: document.getElementById('addDialogContainer'),
-};
-
-/**
- * Toggles the visibility of the add location dialog box.
- */
-function toggleAddDialog() {
-  weatherApp.addDialogContainer.classList.toggle('visible');
-}
-
-
-function updateLocations(country) {
-  //const data = JSON.stringify(locations);
-  localStorage.setItem('locationList', country);
-}
-
 
 /**
  * Search country from user input.
@@ -71,6 +53,48 @@ function searchCountry(){
   });
 }
 
+/**
+ * Search country from user button input.
+ */
+function searchCountryFromButton(button){
+  console.log(button);
+  const spinner = document.querySelector('.card-spinner');
+  spinner.style.display="block";
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://restcountries-v1.p.rapidapi.com/name/"+button,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+      "x-rapidapi-key": "379758d4a7mshcf9c05972f98022p1737f3jsne0e6815fcba2"
+    },
+    success: function (response) {
+
+      $.ajax({
+        url: 'home/location',
+        type: 'POST',
+        data: {
+          'object': response,
+        },
+        dataType: 'json',
+        success: function (data) {
+          $("#about").after(data.html);
+          spinner.style.display="none";
+          addToLocalStorageString('locationList',button,',');
+          $("#results .input-group").append( "<button type=\"button\" class=\"btn btn-primary\">"+button+"</button>" );
+        }, error: function (data) {
+
+        }
+      });
+    }
+  };
+  $.ajax(settings).done(function (response) {
+  });
+}
+/**
+ * Add object into local storage - specific storage name, value and delimiter.
+ */
 var addToLocalStorageString = function (name, value, delimiter) {
   // Get the existing data
   var existing = localStorage.getItem(name);
@@ -81,117 +105,8 @@ var addToLocalStorageString = function (name, value, delimiter) {
 };
 
 /**
- * Event handler for butDialogAdd, adds the selected location to the list.
+ * Load cache items from storage and show them in #result container.
  */
-function addLocation() {
-  // Hide the dialog
-  toggleAddDialog();
-  // Get the selected city
-  const select = document.getElementById('selectCityToAdd');
-  const selected = select.options[select.selectedIndex];
-  const geo = selected.value;
-  const label = selected.textContent;
-  const location = {label: label, geo: geo};
-  // Create a new card & get the weather data from the server
-  const card = getForecastCard(location);
-  getForecastFromNetwork(geo).then((forecast) => {
-    renderForecast(card, forecast);
-  });
-  // Save the updated list of selected cities.
-  weatherApp.selectedLocations[geo] = location;
-  saveLocationList(weatherApp.selectedLocations);
-}
-
-/**
- * Event handler for remove city card.
- *
- * @param {Event} evt
- */
-function removeLocation() {
-  console.log(evt);
-  const parent = evt.srcElement.parentElement;
-  parent.remove();
-
-}
-
-/**
- * Get's the cached forecast data from the caches object.
- *
- * @param {string} coords Location object to.
- * @return {Object} The weather forecast, if the request fails, return null.
- */
-function getForecastFromCache(coords) {
-  // CODELAB: Add code to get weather forecast from the caches object.
-  if (!('caches' in window)) {
-    return null;
-  }
-  const url = `${window.location.origin}/forecast/${coords}`;
-  return caches.match(url)
-      .then((response) => {
-        if (response) {
-          return response.json();
-        }
-        return null;
-      })
-      .catch((err) => {
-        console.error('Error getting data from cache', err);
-        return null;
-      });
-}
-
-
-
-/**
- * Gets the latest weather forecast data and updates each card with the
- * new data.
- */
-function updateData() {
-  Object.keys(weatherApp.selectedLocations).forEach((key) => {
-    const location = weatherApp.selectedLocations[key];
-    const card = getForecastCard(location);
-    // CODELAB: Add code to call getForecastFromCache
-    getForecastFromCache(location.geo)
-        .then((forecast) => {
-          renderForecast(card, forecast);
-        });
-    // Get the forecast data from the network.
-    getForecastFromNetwork(location.geo)
-        .then((forecast) => {
-          renderForecast(card, forecast);
-        });
-  });
-}
-
-/**
- * Saves the list of locations.
- *
- * @param {Object} locations The list of locations to save.
- */
-function saveLocationList(locations) {
-  const data = JSON.stringify(locations);
-  localStorage.setItem('locationList', data);
-}
-
-
-
-/**
- * Loads the list of saved location.
- *
- * @return {Array}
- */
-function loadLocationList() {
-  let locations = localStorage.getItem('locationList');
-  if (locations) {
-    try {
-      locations = JSON.parse(locations);
-    } catch (ex) {
-      locations = {};
-    }
-  }
-
-  return locations;
-}
-
 function loadCacheItems(){
   let locations = localStorage.getItem('locationList');
 
@@ -201,42 +116,36 @@ function loadCacheItems(){
       document.querySelector('#results').style.display = "block";
       var i;
       for (i = 0; i < cacheItems.length; i++) {
-          $("#results .input-group").append( "<button type=\"button\" class=\"btn btn-primary\">"+cacheItems[i]+"</button>" );
+          $("#results .input-group").append( "<button type=\"button\" class=\"btn_country btn btn-primary\">"+cacheItems[i]+"</button>" );
       }
     } catch (ex) {
       locations = {};
     }
   }
 }
-
+/**
+ * Event for remove parent element
+ */
   $(document).on('click','.remove-city',function (e) {
     $(this).parent('div').remove();
   });
-
-
+/**
+ * Event for search from button click
+ */
+$(document).on('click','.btn_country',function (e) {
+  let element = $(this).text();
+  searchCountryFromButton(element);
+});
 
 /**
  * Initialize the app, gets the list of locations from local storage, then
  * renders the initial data.
  */
 function init() {
-  // Get the location list, and update the UI.
-  weatherApp.selectedLocations = loadLocationList();
-  //updateData();
   //Function for loading cache items
   loadCacheItems();
-  // Set up the event handlers for all of the buttons.
-  document.getElementById('butRefresh').addEventListener('click', updateData);
-  document.getElementById('butAdd').addEventListener('click', toggleAddDialog);
-  document.getElementById('butDialogCancel')
-      .addEventListener('click', toggleAddDialog);
-  document.getElementById('butDialogAdd')
-      .addEventListener('click', addLocation);
-
-  // Custom code
   document.getElementById('search_for_country')
       .addEventListener('click', searchCountry);
-
 }
 
 init();
